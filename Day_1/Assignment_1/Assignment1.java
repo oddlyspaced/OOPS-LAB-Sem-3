cff687639464383cd7c5223e640d1a85c4c7f414
oddlyspaced <oddlyspaced1@gmail.com>
2021-01-21 10:09:46 +0530

    Init with Assignment 1

---
+++ Day_1/Assignment_1/Assignment1.java
@@ -0,0 +1,14 @@
+
+// WAP to print multiple of 2
+
+import java.util.Scanner;
+
+public class Assignment1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.print(n * 2);
+        scanner.close();
+    }
+}
+
